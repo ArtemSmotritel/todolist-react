381efffece779ff1ec780b50118f6758f3b80b9d
ArtemSmotritel <artemsmotritel@gmail.com>
2022-06-07 18:09:35 +0300

    some bad sidebar. no content positioning on the page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import List from "./List/List";
 import NewTaskForm from "./NewTaskForm/NewTaskForm";
+import Sidebar from "./TodoListSidebar/Sidebar";
 
 function App() {
   return (
     <>
+      <Sidebar />
       <List />
       <NewTaskForm />
     </>
--- src/TodoListSidebar/Sidebar.js
+++ src/TodoListSidebar/Sidebar.js
@@ -1,5 +1,25 @@
 import React from "react";
+import "./Sidebar.css";
 
-function Sidebar(params) {
-    
+export default function Sidebar(params) {
+  return (
+    <aside className="sidebar">
+      <h1 className="sidebar__main-header">TODO list</h1>
+      <div className="sidebar__checkbox-container">
+        <input
+          className="sidebar__checkbox"
+          type="checkbox"
+          id="sidebar-toggle"
+        ></input>
+        <label className="sidebar__checkbox-label" htmlFor="sidebar-toggle">
+          Show all tasks
+        </label>
+      </div>
+      <h2 className="sidebar__section-header">Your lists</h2>
+      <ul className="list-of-lists">
+        <li className="list-of-lists__item">Easy tasks</li>
+        <li className="list-of-lists__item">Hard tasks</li>
+      </ul>
+    </aside>
+  );
 }
