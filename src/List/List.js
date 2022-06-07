9646e28f4d64958c6b103255ce1045d8c3c55b17
ArtemSmotritel <artemsmotritel@gmail.com>
2022-06-07 15:38:15 +0300

    simple section header component

--- src/List/List.js
+++ src/List/List.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./List.css";
 
 export default function List(params) {
-  return (
-    <section className="list">
-      <h2 className="list__name">Undone tasks</h2>
-    </section>
+  return (    
+      <section className="list">
+        <h3 className="list__name">Undone tasks</h3>
+      </section>    
   );
 }
---
+++ src/SharedComponents/SectionHeader.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './SectionHeader.css';
+
+export default function SectionHeader({title}) {
+    return (
+        <h2 className="title">{title}</h2>
+    )
+}
