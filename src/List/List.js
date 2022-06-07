04c1982866d394544ff35fb2af937ca431544cb7
ArtemSmotritel <artemsmotritel@gmail.com>
2022-06-07 16:23:50 +0300

    simple task component

--- src/List/List.js
+++ src/List/List.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./List.css";
+import Task from "./Task/Task";
 
 export default function List(params) {
   return (    
       <section className="list">
         <h3 className="list__name">Undone tasks</h3>
+        <Task />
       </section>    
   );
 }
---
+++ src/List/Task/Task.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Task.css";
+import deleteImg from "./trash-bin.png";
+
+export default function Task(params) {
+  return (
+    <section className="task" id="1">
+      <img src={deleteImg} alt="a trash can to delete the task" className="task__delete"/>
+      <div className="task__status">
+        <input type="checkbox" name="done" className="task__checkbox" id="task-1"></input>
+        <label className="task__name" htmlFor="task-1">To break</label>
+      </div>
+      <p className="task__date">Due date: <span className="task__due-date">6/9/22</span></p>
+      <p className="task__description">I want to break free</p>
+    </section>
+  );
+}
