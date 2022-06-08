d485e9b2c9430ab12096aa56612b13674529a2f4
ArtemSmotritel <artemsmotritel@gmail.com>
2022-06-08 17:00:02 +0300

    almost add list change handling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,70 @@ import "./App.css";
 import List from "./List/List";
 import NewTaskForm from "./NewTaskForm/NewTaskForm";
 import Sidebar from "./TodoListSidebar/Sidebar";
+import { useState } from "react";
+
+const today = new Date();
+const yesterday = new Date(new Date().setDate(today.getDate() - 1));
+const tomorrow = new Date(new Date().setDate(today.getDate() + 1));
+
+const tasks = [
+  {
+    id: 1,
+    name: "To eat in a restaurant",
+    description: "to eat something really tasty",
+    done: true,
+    due_date: yesterday,
+    list_id: 1,
+  },
+  {
+    id: 6,
+    name: "To run",
+    description: "to run away from",
+    done: false,
+    due_date: today,
+    list_id: 1,
+  },
+  {
+    id: 2,
+    name: "To walk",
+    description:
+      "to have a lonely long walk by the beach. Visit few friends, go shopping",
+    done: false,
+    list_id: 1,
+  },
+  {
+    id: 3,
+    name: "To do this task by nightfall",
+    description:
+      "to work hard and persistently. Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore cum consectetur eveniet, temporibus quas doloribus at inventore ipsum alias nam velit exercitationem vel delectus fuga, voluptatibus libero sapiente soluta rem. ",
+    done: false,
+    due_date: yesterday,
+    list_id: 2,
+  },
+  {
+    id: 4,
+    name: "Chill",
+    description:
+      "to chill. Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore cum consectetur eveniet, temporibus quas doloribus at inventore ipsum alias nam velit exercitationem vel delectus fuga, voluptatibus libero sapiente soluta rem. ",
+    done: false,
+    due_date: yesterday,
+    list_id: 2,
+  },
+  { id: 5, name: "To sleep", done: false, due_date: tomorrow, list_id: 1 },
+];
 
 function App() {
+  const [listId, setListId] = useState(1);
+  
+  const onListChange = id => {
+    setListId(id);
+  }
+
   return (
     <>
-      <Sidebar />
+      <Sidebar onListChange={onListChange}/>
       <main>
-        <List />
+        <List list={tasks.filter((t) => t.list_id === listId)} />
         <NewTaskForm />
       </main>
     </>
--- src/TodoListSidebar/Sidebar.js
+++ src/TodoListSidebar/Sidebar.js
@@ -2,6 +2,16 @@ import React from "react";
 import "./Sidebar.css";
 
 export default function Sidebar(params) {
+  const { onListChange } = params;
+  
+  const handleCheck = (e) => {
+    console.log("changed");
+  };
+
+  const handleListChange = (e) => {
+    onListChange(2);
+  };
+
   return (
     <aside className="sidebar">
       <h1 className="sidebar__main-header">TODO list</h1>
@@ -10,6 +20,7 @@ export default function Sidebar(params) {
           className="sidebar__checkbox"
           type="checkbox"
           id="sidebar-toggle"
+          onChange={handleCheck}
         ></input>
         <label className="sidebar__checkbox-label" htmlFor="sidebar-toggle">
           Show all tasks
@@ -17,8 +28,12 @@ export default function Sidebar(params) {
       </div>
       <h2 className="sidebar__section-header">Your lists</h2>
       <ul className="list-of-lists">
-        <li className="list-of-lists__item">Easy tasks</li>
-        <li className="list-of-lists__item">Hard tasks</li>
+        <li className="list-of-lists__item" onClick={handleListChange}>
+          Easy tasks
+        </li>
+        <li className="list-of-lists__item" onClick={handleListChange}>
+          Hard tasks
+        </li>
       </ul>
     </aside>
   );
