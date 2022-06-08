45561575e8c68233a13822e2127497eed8362d5b
ArtemSmotritel <artemsmotritel@gmail.com>
2022-06-08 12:15:55 +0300

    not-static task component

--- src/List/Task/Task.js
+++ src/List/Task/Task.js
@@ -2,9 +2,39 @@ import React from "react";
 import "./Task.css";
 import deleteImg from "./trash-bin.png";
 
+const task = {
+  id: 1,
+  name: "To eat in a restaurant lorem",
+  description: "to eat something really tasty",
+  done: true,
+  due_date: new Date(),
+  list_id: 1,
+};
+
+const isOverdue = (dueDateStringOrDate) => {
+  const today = new Date();
+  const dueDate = dueDateStringOrDate ? new Date(dueDateStringOrDate) : "";
+
+  if (!dueDate || dueDate.setHours(23, 59, 59) >= today) {
+    return "";
+  }
+  return " task__date_overdue";
+};
+
+const formatDate = (dueDateStringOrDate) => {
+  const dueDate = dueDateStringOrDate ? new Date(dueDateStringOrDate) : "";
+
+  let dateFormmated = dueDate
+    ? ": " + Intl.DateTimeFormat("en-US").format(dueDate)
+    : " is not set";
+
+  return dateFormmated;
+};
+
 export default function Task(params) {
+  const dueDateClass = "task__due-date" + isOverdue(task.due_date);
   return (
-    <section className="task" id="1">
+    <section className="task" id={task.id}>
       <img
         src={deleteImg}
         alt="a trash can to delete the task"
@@ -18,13 +48,22 @@ export default function Task(params) {
           id="task-1"
         ></input>
         <label className="task__name" htmlFor="task-1">
-          To break
+          {task.name}
         </label>
       </div>
-      <p className="task__date">
-        Due date: <span className="task__due-date">6/9/22</span>
-      </p>
-      <p className="task__description">I want to break free</p>
+      {task.description ? (
+        <p className="task__description">{task.description}</p>
+      ) : (
+        ""
+      )}
+      {task.due_date ? (
+        <p className="task__date">
+          Due date:{" "}
+          <span className={dueDateClass}>{formatDate(task.due_date)}</span>
+        </p>
+      ) : (
+        ""
+      )}
     </section>
   );
 }
