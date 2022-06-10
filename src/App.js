add65f2bbb4fbcf5fe00f5e55e4951cc79c91263
ArtemSmotritel <artemsmotritel@gmail.com>
2022-06-10 18:05:28 +0300

    extract all server-communication functions to separate file

--- src/App.js
+++ src/App.js
@@ -3,45 +3,13 @@ import List from "./List/List";
 import NewTaskForm from "./NewTaskForm/NewTaskForm";
 import Sidebar from "./TodoListSidebar/Sidebar";
 import { useState, useEffect } from "react";
-import axios from "axios";
-
-const urlTaskId = (id) => `/tasks/${id}`;
-const urlListId = (id) => `/lists/${id}/tasks?all=true`;
-
-async function makeRequest(url, method = "get", data) {
-  const reqConfig = {
-    method,
-    baseURL: "http://localhost:3001",
-    url,
-  };
-  if (data && method !== "get") reqConfig.data = data;
-
-  const response = await axios(reqConfig);
-  return response;
-}
-
-async function getLists() {
-  const { data } = await makeRequest("/dashboard");
-  return data.undone_tasks_by_list;
-}
-
-async function getListById(id) {
-  const { data } = await makeRequest(urlListId(id));
-  return data;
-}
-
-async function updateTaskOnServer(id, updatedFields) {
-  await makeRequest(urlTaskId(id), "patch", updatedFields);
-}
-
-async function deleteTaskOnServer(id) {
-  await makeRequest(urlTaskId(id), "delete");
-}
-
-async function addTaskOnServer(task, listId) {
-  const { data } = await makeRequest(urlListId(listId), "post", task);
-  return data;
-}
+import {
+  getLists,
+  getListById,
+  updateTaskOnServer,
+  deleteTaskOnServer,
+  addTaskOnServer,
+} from "./serverFunctions";
 
 export default function App() {
   const [listId, setListId] = useState(1),
