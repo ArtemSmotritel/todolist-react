460737001e889a8a8795a249e6699d3fd0a747c2
ArtemSmotritel <artemsmotritel@gmail.com>
2022-06-13 12:11:15 +0300

    highlight selected list in sidebar

--- src/TodoListSidebar/ListItem.js
+++ src/TodoListSidebar/ListItem.js
@@ -1,7 +1,9 @@
+import './ListOfLists.css';
+
 export default function ListItem(params) {
-  const { name, id } = params;
+  const { name, id, highlighted } = params;
   return (
-    <li className="list-of-lists__item" id={`list-${id}`}>
+    <li className={`list-of-lists__item ${highlighted === id && "list-of-lists__item_highlighted"}`} id={`list-${id}`}>
       {name}
     </li>
   );
--- src/TodoListSidebar/Sidebar.js
+++ src/TodoListSidebar/Sidebar.js
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import SectionHeader from "../SharedComponents/SectionHeader";
 import ListItem from "./ListItem";
 import "./Sidebar.css";
 
 export default function Sidebar(params) {
   const { onListIdChange, onToggleDoneTasks, listOfLists, onTodayTasks } = params;
+  const [highlightedList, setHighlightedList] = useState(1);
 
   const handleCheck = (e) => {
     const showAllTasks = e.target.checked;
@@ -11,8 +13,9 @@ export default function Sidebar(params) {
   };
 
   const handleListChange = (e) => {
-    if (e.target.id) {
+    if (e.target.id) {      
       const id = +e.target.id.split("-")[1];
+      setHighlightedList(id);
       onListIdChange(id);
     }
   };
@@ -35,7 +38,7 @@ export default function Sidebar(params) {
       <SectionHeader title={"Your lists"} />
       <ul className="list-of-lists" onClick={handleListChange}>
         {listOfLists.map((l) => (
-          <ListItem name={l.name} id={l.id} key={l.id}/>
+          <ListItem name={l.name} id={l.id} key={l.id} highlighted={highlightedList}/>
         ))}
       </ul>
     </aside>
