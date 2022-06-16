f48ef6187300cb778b8c65573518bbeeb77280de
ArtemSmotritel <artemsmotritel@gmail.com>
2022-06-16 14:41:49 +0300

    add default empty structure for redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from './store';
+
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
       <Routes>
         <Route path="*" element={<App />} />
       </Routes>
+    </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
